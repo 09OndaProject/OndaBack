3300fd701c5b9919e1c6d333002d5fc153217953
LucasKim4321 <xowls0131@naver.com>
2025-06-02 18:33:47 +0900

    🐛fix : 수정

--- apps/reviews/models.py
+++ apps/reviews/models.py
@@ -2,14 +2,14 @@ from django.conf import settings
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-from apps.meet.models import Meet
+# from apps.meet.models import Meet
 
 
 class Review(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="reviews"
     )
-    meet = models.ForeignKey(Meet, on_delete=models.CASCADE, related_name="reviews")
+    # meet = models.ForeignKey(Meet, on_delete=models.CASCADE, related_name="reviews")
     rating = models.PositiveSmallIntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
@@ -20,7 +20,8 @@ class Review(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        unique_together = ["user", "meet"]
+        # unique_together = ["user", "meet"]
+        pass
 
-    def __str__(self):
-        return f"{self.user} - {self.meet} ({self.rating}점)"
+    # def __str__(self):
+    # return f"{self.user} - {self.meet} ({self.rating}점)"
