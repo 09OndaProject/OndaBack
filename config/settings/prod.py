2ef3c7c3fae84c5473d4734a3f4aeb3583a3af7c
LucasKim4321 <xowls0131@naver.com>
2025-06-09 16:39:07 +0900

    🐛fix : 배포 설정 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -7,8 +7,8 @@ DEBUG = False
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    "43.203.181.6",
-    "wistar.n-e.kr",
+    "13.209.4.19",
+    # "wistar.n-e.kr",
 ]  # EC2 퍼블릭 IP
 
 # 시크릿 키를 ENV 변수에 저장된 딕셔너리에서 가져옵니다. 만약 파일에서 읽어온 시크릿 키가 존재하지 않는다면 50자리의 무작위 문자열을 반환합니다.
@@ -29,39 +29,39 @@ DATABASES = {
 }
 
 # S3 사용시 설정
-STORAGES = {
-    "default": {
-        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-        "OPTIONS": {
-            "access_key": ENV.get("S3_ACCESS_KEY", ""),
-            "secret_key": ENV.get("S3_SECRET_ACCESS_KEY", ""),
-            "bucket_name": ENV.get("S3_STORAGE_BUCKET_NAME", ""),
-            "region_name": ENV.get("S3_REGION_NAME", ""),
-            "custom_domain": f'{ENV.get("S3_STORAGE_BUCKET_NAME", "")}.s3.amazonaws.com',
-            "location": "media",
-            "default_acl": "public-read",
-        },
-    },
-    "staticfiles": {
-        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
-        "OPTIONS": {
-            "access_key": ENV.get("S3_ACCESS_KEY", ""),
-            "secret_key": ENV.get("S3_SECRET_ACCESS_KEY", ""),
-            "bucket_name": ENV.get("S3_STORAGE_BUCKET_NAME", ""),
-            "region_name": ENV.get("S3_REGION_NAME", ""),
-            "custom_domain": f'{ENV.get("S3_STORAGE_BUCKET_NAME", "")}.s3.amazonaws.com',
-            "location": "static",
-            "default_acl": "public-read",
-        },
-    },
-}
+# STORAGES = {
+#     "default": {
+#         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+#         "OPTIONS": {
+#             "access_key": ENV.get("S3_ACCESS_KEY", ""),
+#             "secret_key": ENV.get("S3_SECRET_ACCESS_KEY", ""),
+#             "bucket_name": ENV.get("S3_STORAGE_BUCKET_NAME", ""),
+#             "region_name": ENV.get("S3_REGION_NAME", ""),
+#             "custom_domain": f'{ENV.get("S3_STORAGE_BUCKET_NAME", "")}.s3.amazonaws.com',
+#             "location": "media",
+#             "default_acl": "public-read",
+#         },
+#     },
+#     "staticfiles": {
+#         "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+#         "OPTIONS": {
+#             "access_key": ENV.get("S3_ACCESS_KEY", ""),
+#             "secret_key": ENV.get("S3_SECRET_ACCESS_KEY", ""),
+#             "bucket_name": ENV.get("S3_STORAGE_BUCKET_NAME", ""),
+#             "region_name": ENV.get("S3_REGION_NAME", ""),
+#             "custom_domain": f'{ENV.get("S3_STORAGE_BUCKET_NAME", "")}.s3.amazonaws.com',
+#             "location": "static",
+#             "default_acl": "public-read",
+#         },
+#     },
+# }
 
 # # custom_domain이 설정되어 있으면, 이 도메인을 사용해서 URL을 생성
 # # 그렇지 않으면 기본 Amazon S3 URL (s3.{region}.amazonaws.com/{bucket}/...)을 사용
 
 # Static, Media URL 수정
-MEDIA_URL = f"https://{ENV.get('S3_STORAGE_BUCKET_NAME')}.s3.amazonaws.com/media/"
-STATIC_URL = f"https://{ENV.get('S3_STORAGE_BUCKET_NAME')}.s3.amazonaws.com/static/"
+# MEDIA_URL = f"https://{ENV.get('S3_STORAGE_BUCKET_NAME')}.s3.amazonaws.com/media/"
+# STATIC_URL = f"https://{ENV.get('S3_STORAGE_BUCKET_NAME')}.s3.amazonaws.com/static/"
 
 
 # # 기본 이미지 url 설정
@@ -88,11 +88,6 @@ EMAIL_HOST_PASSWORD = ENV.get("EMAIL_HOST_PASSWORD", "")
 
 
 # OAuth
-NAVER_CLIENT_ID = ENV.get("NAVER_CLIENT_ID", "")
-NAVER_CLIENT_SECRET = ENV.get("NAVER_CLIENT_SECRET", "")
-
 KAKAO_REST_API_KEY = ENV.get("KAKAO_REST_API_KEY", "")
 KAKAO_CLIENT_SECRET = ENV.get("KAKAO_CLIENT_SECRET", "")
 
-GOOGLE_CLIENT_ID = ENV.get("GOOGLE_CLIENT_ID", "")
-GOOGLE_CLIENT_SECRET = ENV.get("GOOGLE_CLIENT_SECRET", "")
