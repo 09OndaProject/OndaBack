75da1f922ff8663262f2a01b12f2fddd77000b4a
GwonDohyeon <125738341+GwonDohyeon@users.noreply.github.com>
2025-06-11 16:05:31 +0900

    🐛fix : status 필드 프로퍼티로 대체
    # Conflicts:
    #	apps/meet/models.py

--- apps/meet/models.py
+++ apps/meet/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 from apps.options.models import *
 from utils.models import TimestampModel
+from django.utils import timezone
 
 # from apps.file.models import *
 User = get_user_model()
@@ -16,25 +17,32 @@ STATUS_CHOICES = (
 
 
 class Meet(TimestampModel):
+    @property
+    def status(self):
+        if self.application_deadline and self.application_deadline < timezone.now():
+            return "마감"
+        return "모집중"
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
     area = models.ForeignKey(Area, on_delete=models.SET_NULL, null=True)
     digital_level = models.ForeignKey(
         DigitalLevel, on_delete=models.SET_NULL, null=True
     )
     interest = models.ForeignKey(Interest, on_delete=models.SET_NULL, null=True)
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
     date = models.DateField(null=True, blank=True)
     time = models.TimeField(null=True, blank=True)
     location = models.CharField(max_length=255, null=True, blank=True)
     contact = models.CharField(max_length=255, null=True, blank=True)
     max_people = models.IntegerField(null=True, blank=True)
     current_people = models.IntegerField(default=0)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="모집중")
-    # file = models.OneToOneField(File,on_delete=models.CASCADE)
     application_deadline = models.DateTimeField()
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    # file = models.OneToOneField(File,on_delete=models.CASCADE)
 
     def __str__(self):
         return f"[{self.pk}] {self.title}"
