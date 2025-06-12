815c366fb2ccff097bd36d34a2975d47db09efc5
LucasKim4321 <xowls0131@naver.com>
2025-06-12 14:20:24 +0900

    Merge pull request #21 from 09OndaProject/feature/community

--- apps/posts/serializers.py
+++ apps/posts/serializers.py
@@ -25,4 +25,10 @@ class PostSerializer(serializers.ModelSerializer):
             "created_at",
             "updated_at",
             "nickname",
+            "is_mine",
         ]
+
+        def get_is_mine(self, obj):
+            # context에 request가 있어야 함 (APIView에서 serializer에 전달됨)
+            request = self.context.get('request')
+            return request and request.user.is_authenticated and obj.user == request.user
