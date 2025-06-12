067ff534af864edc7b78a5481a272777bfd148e8
LucasKim4321 <xowls0131@naver.com>
2025-06-12 14:26:36 +0900

    🐛fix : 포맷팅 수정

--- apps/posts/serializers.py
+++ apps/posts/serializers.py
@@ -30,5 +30,7 @@ class PostSerializer(serializers.ModelSerializer):
 
         def get_is_mine(self, obj):
             # context에 request가 있어야 함 (APIView에서 serializer에 전달됨)
-            request = self.context.get('request')
-            return request and request.user.is_authenticated and obj.user == request.user
+            request = self.context.get("request")
+            return (
+                request and request.user.is_authenticated and obj.user == request.user
+            )
