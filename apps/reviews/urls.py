e26c316981ac190f3b2060cedc7d40e6c39c3a3c
gkw314 <go980314@gmail.com>
2025-06-14 02:09:55 +0900

    💡chore : 리뷰, 옵션 앱 ci수정

--- apps/reviews/urls.py
+++ apps/reviews/urls.py
@@ -1,14 +1,23 @@
 from django.urls import path
+
 from .views import (
+    MyReviewListView,
     ReviewDetailView,
     ReviewListCreateView,
     ReviewSummaryView,
-    MyReviewListView,
 )
 
 urlpatterns = [
-    path("meets/<int:meet_id>/reviews", ReviewListCreateView.as_view(), name="review-list-create"),
-    path("meets/<int:meet_id>/reviews/summary", ReviewSummaryView.as_view(), name="review-summary"),
+    path(
+        "meets/<int:meet_id>/reviews",
+        ReviewListCreateView.as_view(),
+        name="review-list-create",
+    ),
+    path(
+        "meets/<int:meet_id>/reviews/summary",
+        ReviewSummaryView.as_view(),
+        name="review-summary",
+    ),
     path("reviews/<int:pk>", ReviewDetailView.as_view(), name="review-detail"),
     path("users/reviews", MyReviewListView.as_view(), name="my-review-list"),
 ]
