ba1a55793b4d5ae5f86e8932730e32f165c4dd36
LucasKim4321 <xowls0131@naver.com>
2025-06-17 18:02:29 +0900

    🐛fix : post url 수정

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -4,15 +4,17 @@ from . import views
 
 urlpatterns = [
     # 게시글 CRUD
-    path("", views.PostListCreateView.as_view()),  # GET/POST
-    path("<int:pk>/", views.PostDetailView.as_view()),  # GET/PATCH/DELETE
+    path("posts", views.PostListCreateView.as_view()),  # GET/POST
+    path("posts/<int:pk>", views.PostDetailView.as_view()),  # GET/PATCH/DELETE
     # 댓글/대댓글 CRUD
-    path("<int:post_id>/comments/", views.CommentListCreateView.as_view()),  # GET/POST
     path(
-        "<int:post_id>/comments/<int:pk>/", views.CommentDetailView.as_view()
+        "posts/<int:post_id>/comments", views.CommentListCreateView.as_view()
+    ),  # GET/POST
+    path(
+        "posts/<int:post_id>/comments/<int:pk>", views.CommentDetailView.as_view()
     ),  # GET/PATCH/DELETE
     # 좋아요
-    path("<int:post_id>/like/", views.LikeToggleView.as_view()),  # POST
+    path("posts/<int:post_id>/like", views.LikeToggleView.as_view()),  # POST
     path("posts", views.PostListCreateView.as_view()),  # GET/POST
     path("posts/<int:pk>", views.PostDetailView.as_view()),  # GET/PATCH/DELETE
 ]
