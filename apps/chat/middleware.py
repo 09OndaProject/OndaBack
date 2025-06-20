531c6418d241248a52324fcbd1e858f518e0a3ed
LucasKim4321 <xowls0131@naver.com>
2025-06-20 20:24:40 +0900

    🐛fix : asgi 설정 변경

--- apps/chat/middleware.py
+++ apps/chat/middleware.py
@@ -3,6 +3,7 @@ from urllib.parse import parse_qs
 from channels.db import database_sync_to_async
 from channels.middleware import BaseMiddleware
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
 from rest_framework_simplejwt.tokens import AccessToken
 
 User = get_user_model()
@@ -29,7 +30,6 @@ class JWTAuthMiddleware(BaseMiddleware):
             user = await get_user_from_token(token)
             scope["user"] = user if user else AnonymousUser()
         else:
-            from django.contrib.auth.models import AnonymousUser
 
             scope["user"] = AnonymousUser()
 
