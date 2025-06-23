2ffec8421201c62bc02f0045f1633c78dd338402
LucasKim4321 <xowls0131@naver.com>
2025-06-23 17:40:30 +0900

    🚑hotfix : 포멧팅

--- apps/user/utils/jwt_token.py
+++ apps/user/utils/jwt_token.py
@@ -46,9 +46,7 @@ def set_refresh_token_cookie(response, refresh_token, request):
         # samesite="Strict",  # 출처가 다르면 어떤 요청도 쿠키 전송 안 함
         # samesite="Lax",  # 출처가 다른 요청 중, GET 요청만 쿠키 허용
         # samesite="None",  # 출처가 달라도 모든 요청에 쿠키를 전송 허용
-        samesite=(
-            "Strict" if is_prod else "None"
-        ),
+        samesite=("Strict" if is_prod else "None"),
         # path="/api/users/token",  # 필요한 경로에만 쿠키 사용
         domain=".ondamoim.com" if is_prod else None,  # 특정 도메인에만 쿠키 사용
         max_age=60 * 60 * 24 * 1,  # 1일 (초 단위)
